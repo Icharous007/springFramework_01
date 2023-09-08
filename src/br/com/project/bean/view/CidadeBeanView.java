661c080c7fa8f2f0cebb92236effdea79d1d9e23
Gustavo.sousa <gustavo.sousa@castgroup.com.br>
2023-09-07 22:46:15 -0300

    cidades parte 2 commit

--- src/br/com/project/bean/view/CidadeBeanView.java
+++ src/br/com/project/bean/view/CidadeBeanView.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 
 import br.com.project.bean.geral.BeanManagedViewAbstract;
 import br.com.project.geral.controller.CidadeController;
+import br.com.project.model.classes.Cidade;
 
 @Controller
 @Scope(value = "session")
@@ -18,5 +19,23 @@ public class CidadeBeanView extends BeanManagedViewAbstract{
 	
 	@Autowired
 	private CidadeController cidadeController;
+	
+	
+	private Cidade cidadeSelecionada = new Cidade();
 
+
+	public Cidade getCidadeSelecionada() {
+		return cidadeSelecionada;
+	}
+
+
+	public void setCidadeSelecionada(Cidade cidadeSelecionada) {
+		this.cidadeSelecionada = cidadeSelecionada;
+	}
+	
+	@Override
+	public String save() throws Exception {
+		System.out.println(cidadeSelecionada.getCidade_nome());
+		return "";
+	}
 }
